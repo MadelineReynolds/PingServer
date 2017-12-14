87fede12a709a625ae5af69a42a70b5f18d5b4bd
m195334 <m195334@usna.edu>
2017-12-14 14:58:37 -0800

    everything but check

--- client.py
+++ client.py
@@ -1,22 +1,39 @@
 #!/usr/bin/python3
 import sys
+import base64
+import hashlib
 from socket import *
 import threading
 
-def pingServer(csock):
+def carry_around_add(a, b):
+  c = a + b
+  return (c & 0xffff) + (c >> 16)
+
+def calculateCheckSum(message):
+  length = len(message)
+  if(length%2 != 0):
+    message = message + '\x00'
+  #https://wiki.python.org/moin/BitwiseOperators
+  #bitString = byteArray(message)
+  s = 0
+  for i in range(0, len(message), 2):
+    w = ord(message[i]) + (ord(message[i+1]) << 8)
+    s = carry_around_add(s, w)
+  return ~s & 0xffff
+
+def pingServer(csock, message):
   try:
-    data = "This is the hard-coded thesage"
-    #chk = checksum(data)
-    msg_type = '\x08' # ICMP Echo Request
-    msg_code = '\x00' # must be zero
-    msg_checksum_padding = '\x00\x00' 
-    rest_header = '\x00\x01\x00\x01' 
-    entire_message = msg_type + msg_code + msg_checksum_padding + rest_header + data
-    entire_chk = '0xff6c'
-    header = "Type: 8\r\nCode: 0\r\nChecksum: " + str(entire_chk) + "\r\nIdentifier (BE): 19\r\nIdentifier (LE): 19\r\nSequence number (BE): 1\r\nSequence number (LE): 256\r\n"
-    icmpPack = header + "Data: " + data + "\r\n\r\n"
-    print(icmpPack)
-    csock.send(icmpPack.encode())
+    data = message
+    pingType = '\x08' 
+    pingCode = '\x00' 
+    pingCheckPad = '\x00\x00' 
+    pingRestHead = '\x00\x01\x00\x01' 
+    entireMessage = pingType + pingCode + pingCheckPad + pingRestHead + data
+    chk = calculateCheckSum(entireMessage)
+    #chk = hex(chk)
+    print(chk)
+    entireMessage = pingType + pingCode + chk + pingRestHead + data
+    csock.send(entireMessage.encode())
     csock.close()
   except:
     csock.close()
@@ -29,8 +46,13 @@ def main():
   print("Socket created") 
   clientSocket.connect((serverName, serverPort))
   print("Connection achieved")
-  workerThread = threading.Thread(target = pingServer, args=(clientSocket,))
+  message = input('Enter your secret message: ')
+  message = (base64.encodestring(message.encode())).decode()
+  #https://code.tutsplus.com/tutorials/base64-encoding-and-decoding-using-python--cms-25588
+  workerThread = threading.Thread(target = pingServer, args=(clientSocket,
+    message))
   workerThread.start()
+  
   try:
     while (1):
       message = clientSocket.recv(1024)
@@ -38,6 +60,7 @@ def main():
       print(message)
       if len(message) == 0: 
         raise Exception("Client closed unexpectedly")
+  
   except:
     clientSocket.close()
   
--- server.py
+++ server.py
@@ -13,24 +13,21 @@ def readClient(serverSocket):
   try:  
     while True:
       rec_packet, addr = serverSocket.recvfrom(5120)
-      print(rec_packet)
   except:
     serverSocket.close()
-
-def sendClient(csock):
-  csock.send("Ping packet".encode())
               
 def main(serverPort):
   serverSocket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
-
   serverSocket.bind(('localhost',serverPort))
   serverSocket.setsockopt(SOL_IP, IP_HDRINCL, 1)
   print('Ready to serve on port',serverPort)
+  
   try:
       workerThread = threading.Thread(target = readClient, args=(serverSocket,))
       #workerThread2 = threading.Thread(target = sendClient, args=(connectionSocket,)) 
       workerThread.start()
       #workerThread2.start()
+  
   except KeyboardInterrupt:
     connectionSocket.send("Server shutting down now! Thanks for pinging!\n".encode())
     serverSocket.close()
@@ -38,7 +35,6 @@ def main(serverPort):
     print("Shutdown complete... exiting")
     sys.exit()
                   
-       
 if __name__ == "__main__":
   servPort = DEFAULT_PORT
   main(servPort)
